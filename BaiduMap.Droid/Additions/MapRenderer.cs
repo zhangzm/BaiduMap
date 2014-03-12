47e9040bd1395ca6bc8dd078b3600fc0aff20b49
achu <82724824@qq.com>
2014-03-12 09:51:25 +0800

    解决部分警告

---
+++ BaiduMap.Droid/Additions/MapRenderer.cs
@@ -0,0 +1,17 @@
+﻿using Android.Runtime;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Com.Baidu.Platform.Comapi.Map.Base
+{
+    public partial class MapRenderer
+    {
+        [Register("com/baidu/platform/comapi/map/base/MapRenderer$a")]
+        public interface MapRenderer_A
+        {
+            [Register("g", "()V", "")]
+            void G();
+        }
+    }
+}
---
+++ BaiduMap.Droid/Additions/PermissionCheck.cs
@@ -0,0 +1,23 @@
+﻿using Android.Runtime;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Com.Baidu.Mapapi.Utils
+{
+    public partial class PermissionCheck
+    {
+        [Register("com/baidu/mapapi/utils/PermissionCheck$a")]
+        public class PermissionCheck_A : Java.Lang.Object
+        {
+				
+        }
+
+        [Register("com/baidu/mapapi/utils/PermissionCheck$b")]
+        public interface IPermissionCheck_B
+        {
+            [Register("onGetPermissionCheckResult", "(Lcom/baidu/mapapi/utils/PermissionCheck$a;)V", "")]
+             void OnGetPermissionCheckResult(PermissionCheck_A parama);
+        }
+    }
+}
